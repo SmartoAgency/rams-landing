257110a2c66437d791ecdd9832d35cc5ad5ec3e6
Dron2019 <dron_2019@ukr.net>
2023-05-19 10:12:37 +0300

    https://docs.google.com/spreadsheets/d/1jFTTCdk-yPn0eSd98iuOFtEoB1aln1gx-eVOB9HNCWQ/edit#gid=0

--- src/assets/scripts/index-app.js
+++ src/assets/scripts/index-app.js
@@ -523,3 +523,32 @@ document.querySelectorAll('[data-lazy]').forEach(el => {
 })
 
 googleMap();
+
+
+document.querySelectorAll('.web-item').forEach(item => {
+  const button = item.querySelector('[data-circle-with-hover]');
+  const frame = item.querySelector('iframe');
+  const playButtonPath = item.querySelector('[data-circle-with-hover] path');
+  const morphs = {
+      default: playButtonPath.getAttribute('d'),
+      custom: 'M 59 60 L 38 60 C 38 60 38 60 38 60 L 38 40 C 38 40 59 40 59 40 L 59 60 C 59 60 59 60 59 60 Z',
+  }
+  button.addEventListener('click', () => {
+      const isHaveClass = item.classList.toggle('is-playing');
+      if (isHaveClass) {
+          frame.src = frame.dataset.src;
+      } else {
+          frame.removeAttribute('src');
+      }
+      button.remove();
+      // gsap.to(playButtonPath, {
+      //     duration: 0.5,
+      //     ease: 'linear',
+      //     attr: {
+      //         d: isHaveClass ? morphs.custom : morphs.default,
+      //     } 
+      // })
+  }, {
+    once: true
+  })
+})
